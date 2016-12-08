819fe5d8f44c70a1e319762c6c1f25879e5f9366
Taylor Kemper <tjkemper6@gmail.com>
2016-12-07 21:25:11 -0500

    specify namespace for consistency

--- up.sh
+++ up.sh
@@ -8,5 +8,5 @@ kubectl apply -f services/sonar.yaml --namespace operations
 kubectl apply -f deployments/sonar.yaml --namespace operations
 
 # ingress
-kubectl apply -f ingress/sonar.yaml
+kubectl apply -f ingress/sonar.yaml --namespace operations
 
