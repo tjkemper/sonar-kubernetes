987f8697dfd5cf903dc931878660fc411b9d3e40
Taylor Kemper <tjkemper6@gmail.com>
2016-12-07 21:22:25 -0500

    correct up.sh script

--- up.sh
+++ up.sh
@@ -1,13 +1,11 @@
 # secret
-kubectl create secret generic postgres-pwd --from-file=./password --namespace operations
+kubectl apply -f secrets/db-info.yaml --namespace operations
 
 # service
 kubectl apply -f services/sonar.yaml --namespace operations
-kubectl apply -f services/sonar-postgres.yaml --namespace operations
 
 # deployment
 kubectl apply -f deployments/sonar.yaml --namespace operations
-kubectl apply -f deployments/sonar-postgres.yaml --namespace operations
 
 # ingress
 kubectl apply -f ingress/sonar.yaml
